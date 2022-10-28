c8a36329dabefcb03ca75b75b0615adf9a99df6b
Vinícius <95228872+marvigdev@users.noreply.github.com>
2022-10-27 23:06:18 -0300

    coisas

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,31 @@
+const btn = $("#popover-racas-btn");
+const fechar = $("#racas-content-fechar");
+const content = $("#popover-racas-content");
+
+content.hide();
+
+btn.click(() => {
+  content.fadeIn(250);
+});
+
+fechar.click(() => {
+  content.fadeOut(250);
+});
+
+// Inicializa slider de tópicos
 new Glide(".topics", {
   type: "slider",
+  perView: 3,
+  rewind: false,
+  breakpoints: {
+    600: { perView: 1 },
+    800: { perView: 2 },
+  },
+}).mount();
+
+// Inicializa slider de tópicos
+new Glide(".racas-carousel", {
+  type: "carousel",
   perView: 3,
   rewind: false,
   breakpoints: {
